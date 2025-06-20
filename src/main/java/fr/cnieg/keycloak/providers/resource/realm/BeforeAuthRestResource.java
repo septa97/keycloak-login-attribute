d4ce49a98be7c174b4505b55d6f19a0ac181d0dd
septa97 <jancharles.adona@gmail.com>
2025-06-20 23:13:29 +0800

    Use Keycloak's Admin REST API authentication

--- src/main/java/fr/cnieg/keycloak/providers/resource/realm/BeforeAuthRestResource.java
+++ src/main/java/fr/cnieg/keycloak/providers/resource/realm/BeforeAuthRestResource.java
@@ -1,41 +1,80 @@
 package fr.cnieg.keycloak.providers.resource.realm;
 
-import fr.cnieg.keycloak.providers.login.attribute.authenticator.AttributeUsernamePasswordForm;
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.keycloak.common.ClientConnection;
+import org.keycloak.jose.jws.JWSInput;
+import org.keycloak.jose.jws.JWSInputException;
+import org.keycloak.models.ClientModel;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.representations.AccessToken;
 import org.keycloak.services.managers.AppAuthManager;
 import org.keycloak.services.managers.AuthenticationManager;
+import org.keycloak.services.managers.RealmManager;
+import org.keycloak.services.resources.admin.AdminAuth;
 
-import javax.ws.rs.ForbiddenException;
-import javax.ws.rs.NotAuthorizedException;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 
 public class BeforeAuthRestResource {
     private static final Logger logger = Logger.getLogger(BeforeAuthRestResource.class);
+
+    @Context
+    private ClientConnection clientConnection;
+
     private final KeycloakSession session;
-    private final AuthenticationManager.AuthResult auth;
 
     public BeforeAuthRestResource(KeycloakSession session) {
         this.session = session;
-        this.auth = new AppAuthManager.BearerTokenAuthenticator(session).authenticate();
     }
 
     @Path("")
-    public SearchByAttributeRestResource getSearchByAttributeRestResourceAuthenticated() {
-        checkRealmAdmin();
-        return new SearchByAttributeRestResource(session);
+    public SearchByAttributeRestResource getSearchByAttributeRestResourceAuthenticated(@Context final HttpHeaders headers) {
+        AdminAuth auth = authenticateRealmAdminRequest(headers);
+        logger.debug("authenticated admin access for: " + auth.getUser().getUsername());
+
+        SearchByAttributeRestResource searchByAttributeRestResource = new SearchByAttributeRestResource(session);
+        ResteasyProviderFactory.getInstance().injectProperties(searchByAttributeRestResource);
+        return searchByAttributeRestResource;
     }
 
-    private void checkRealmAdmin() {
-        if (auth == null) {
-            throw new NotAuthorizedException("Bearer");
-        } else if (auth.getToken().getRealmAccess() == null || !auth.getToken().getRealmAccess().isUserInRole("admin")) {
-            if (auth.getToken().getRealmAccess() != null) {
-                for (String role : auth.getToken().getRealmAccess().getRoles()) {
-                    logger.info("role: " + role);
-                }
-            }
-            throw new ForbiddenException("Does not have realm admin role");
+    private AdminAuth authenticateRealmAdminRequest(HttpHeaders headers) {
+        String tokenString = AppAuthManager.extractAuthorizationHeaderToken(headers);
+        if (tokenString == null) throw new NotAuthorizedException("Bearer");
+        AccessToken token;
+
+        try {
+            JWSInput input = new JWSInput(tokenString);
+            token = input.readJsonContent(AccessToken.class);
+        } catch (JWSInputException e) {
+            throw new NotAuthorizedException("Bearer token format error");
         }
+        String realmName = token.getIssuer().substring(token.getIssuer().lastIndexOf('/') + 1);
+        RealmManager realmManager = new RealmManager(session);
+        RealmModel realm = realmManager.getRealmByName(realmName);
+        if (realm == null) {
+            throw new NotAuthorizedException("Unknown realm in token");
+        }
+        session.getContext().setRealm(realm);
+
+        AuthenticationManager.AuthResult authResult = new AppAuthManager.BearerTokenAuthenticator(session)
+                .setRealm(realm)
+                .setConnection(clientConnection)
+                .setHeaders(headers)
+                .authenticate();
+
+        if (authResult == null) {
+            logger.debug("Token not valid");
+            throw new NotAuthorizedException("Bearer");
+        }
+
+        ClientModel client = realm.getClientByClientId(token.getIssuedFor());
+        if (client == null) {
+            throw new NotFoundException("Could not find client for authorization");
+        }
+
+        return new AdminAuth(realm, authResult.getToken(), authResult.getUser(), client);
     }
 }
