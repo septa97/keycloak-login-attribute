d7b0d6899ca3dcddc134b5e1dd83903f0a7e6974
septa97 <jancharles.adona@gmail.com>
2025-06-25 13:26:21 +0800

    Modify REST endpoint to cater generic user_id

--- src/main/java/fr/cnieg/keycloak/providers/resource/realm/SearchByAttributeRestResource.java
+++ src/main/java/fr/cnieg/keycloak/providers/resource/realm/SearchByAttributeRestResource.java
@@ -1,6 +1,7 @@
 package fr.cnieg.keycloak.providers.resource.realm;
 
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.UserModel;
 import org.keycloak.models.utils.ModelToRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 public class SearchByAttributeRestResource {
     // TODO: move to a constants file
     private static final String MIN = "min";
+    private static final String USER_ID = "userId";
 
     private final KeycloakSession session;
 
@@ -23,12 +25,18 @@ public class SearchByAttributeRestResource {
     }
 
     @GET
-    @Path("/min/{min}")
+    @Path("/user-id/{userId}")
     @Produces({MediaType.APPLICATION_JSON})
-    public List<UserRepresentation> getUsersByMIN(@PathParam(MIN) String min) {
-        return session.users().searchForUserByUserAttributeStream(session.getContext().getRealm(), MIN, min)
-                .map(userModel -> ModelToRepresentation.toRepresentation(session, session.getContext().getRealm(), userModel))
-                .collect(Collectors.toList());
+    public List<UserRepresentation> getUsersByUserId(@PathParam(USER_ID) String userId) {
+        UserModel user = session.users().getUserByEmail(session.getContext().getRealm(), userId);
+
+        if (user == null) {
+            return session.users().searchForUserByUserAttributeStream(session.getContext().getRealm(), MIN, userId)
+                    .map(userModel -> ModelToRepresentation.toRepresentation(session, session.getContext().getRealm(), userModel))
+                    .collect(Collectors.toList());
+        } else {
+            return List.of(ModelToRepresentation.toRepresentation(session, session.getContext().getRealm(), user));
+        }
     }
 
     @GET
